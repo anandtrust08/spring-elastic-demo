be3ac05e0485fabfb3f0d47022f51c7873942f23
Anand Advani <anand.advani@kuehne-nagel.com>
2019-09-19 17:23:52 +0200

    first commit

---
+++ src/main/java/com/esb/elastic/springelasticdemo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.esb.elastic.springelasticdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HELOO");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
